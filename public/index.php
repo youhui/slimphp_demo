f6264e6814b169a5bdc860e41086ee12176db76f
youhui <>
2019-03-26 10:47:10 +0800

    -

--- public/index.php
+++ public/index.php
@@ -23,5 +23,45 @@ $app->get('/users', function ($request, $response, $args) {
 	return $response->withJson(formatOutput(true, $users, '请求成功'));
 });
 
+$app->get('/users/{id}', function ($request, $response, $args) {
+	$id = $request->getAttribute('id');
+	$db = new DBHandle();
+	$user = $db->getUser($id);
+	return $response->withJson(formatOutput(true, $user, '请求成功'));
+});
+
+$app->post('/users', function ($request, $response, $args) {
+	$body = $request->getParsedBody();
+	$db = new DBHandle();
+	$status = $db->createUser($body);
+	if ($status) {
+		return $response->withJson(formatOutput(true, $status, '用户添加成功'));
+	} else {
+		return $response->withJson(formatOutput(false, $status, '用户添加失败'));
+	}
+});
+
+$app->put('/users', function ($request, $response, $args) {
+	$body = $request->getParsedBody();
+	$db = new DBHandle();
+	$status = $db->updateUser($body);
+	if ($status) {
+		return $response->withJson(formatOutput(true, $status, '用户修改成功'));
+	} else {
+		return $response->withJson(formatOutput(false, $status, '用户修改失败'));
+	}
+});
+
+$app->delete('/users', function ($request, $response, $args) {
+	$body = $request->getParsedBody();
+	$db = new DBHandle();
+	$status = $db->deleteUser($body);
+	if ($status) {
+		return $response->withJson(formatOutput(true, $status, '用户删除成功'));
+	} else {
+		return $response->withJson(formatOutput(false, $status, '用户删除失败'));
+	}
+});
+
 // 运行 app
 $app->run();
